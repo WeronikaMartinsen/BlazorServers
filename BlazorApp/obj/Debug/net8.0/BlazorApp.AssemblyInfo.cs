f09f53e9110329bad675d6108ced4f22915cc3fa
WeronikaMartinsen <121826608+WeronikaMartinsen@users.noreply.github.com>
2024-05-24 12:15:16 +0200

    learning

--- BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
+++ BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2181e33898e2af4ffcdeaa55b19d797735a12d88")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+859d2da83b762680334553df3ee29c0c7ff967b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
